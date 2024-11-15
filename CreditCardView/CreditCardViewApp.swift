9cd622c1068d8eec0e661bfb79b19efe581c457c
Yusuf Aydın <yusayd3825@gmail.com>
2024-11-15 10:24:04 +0300

    first commit

---
+++ CreditCardView/CreditCardViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardViewApp.swift
+//  CreditCardView
+//
+//  Created by yusuf on 15.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct CreditCardViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
